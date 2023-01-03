7b4eb5aaa0a622d95e421ad080bff1c0835500c7
yahyakmail59 <43523989+yahyakmail59@users.noreply.github.com>
2023-01-03 18:05:49 +0200

    Note:
    you need to install node_modules to run this project 
    just init new project to get new project

---
+++ App.js
@@ -0,0 +1,19 @@
+import Login from "./src/screens/login";
+import Home from "./src/screens/home";
+import PrivacyPolice from "./src/screens/privacyPolice";
+import ForgotPassword from "./src/screens/forgotPassword";
+import ContactUs from "./src/screens/contactUs"
+import MobileCourse from "./src/screens/mobileCourse"
+import WebCourse from "./src/screens/webCourse"
+import PhotoshopCourse from "./src/screens/photoshopCourse"
+import Profile from "./src/screens/profile"
+import Navigation from "./src/screens/navigation"
+import { NavigationContainer } from "@react-navigation/native";
+
+
+function App() {
+  return (
+    <Navigation />
+  );
+}
+export default App;
